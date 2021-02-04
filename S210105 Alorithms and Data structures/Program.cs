59cf9c0740b869f889953b5a3c0985ba46f0684b
Jack Howlett <53276208+jackthehat8@users.noreply.github.com>
2021-02-04 15:15:27 +0000

    added linked list

--- S210105 Alorithms and Data structures/Program.cs
+++ S210105 Alorithms and Data structures/Program.cs
@@ -6,93 +6,6 @@
 
 namespace S210105_Alorithms_and_Data_structures
 {
-    class MyGenericClass<T>
-    {
-        private T genericMemberVariable;
-
-        public MyGenericClass(T value)
-        {
-            genericMemberVariable = value;
-        }
-
-        public T getType(T genericParameter)
-        {
-            Console.WriteLine("Parameter type: {0}, value {1}", typeof(T).ToString(), genericParameter);
-            Console.WriteLine("Return type: {0}, value: {1}", typeof(T).ToString(), genericMemberVariable);
-            Console.WriteLine();
-            return genericMemberVariable;
-        }
-
-        public T addOne()
-        {
-            //This was usesd to help add 2 generic values together
-            //https://stackoverflow.com/questions/8122611/c-sharp-adding-two-generic-values
-            if (typeof(T) == typeof(char))
-            {
-                //done separately so it needs to be converted to unicode to add the one
-                dynamic temp1 = Convert.ToInt32(genericMemberVariable);
-                genericMemberVariable = Convert.ToChar(temp1 + 1);
-            }
-            else 
-            {
-                //for strings it concantinates it to the end
-                //others it adds 1 to the current value
-                dynamic temp1 = genericMemberVariable;
-                dynamic temp2 = 1;
-
-                genericMemberVariable = temp1 + temp2;
-            }
-
-            return genericMemberVariable;
-        }
-        public T genericProperty { get; set; }
-    }
-
-    class GenericArray<T>
-    {
-        private T[] array;
-        //inpus the data into the array
-
-        public GenericArray(T[] initalArray)
-        {
-            array = initalArray;
-        }
-
-        public T getData(int columnOne)
-        {
-            if (columnOne < array.GetLength(0) && columnOne >= -1)
-            {
-                return array[columnOne];
-            }
-            else
-            {
-                Console.WriteLine("Error - outside range of data");
-                return default(T);
-            }
-        }
-    }
-
-    class GenericPair<T,Y>
-    {
-        private T itemOne;
-        private Y itemTwo;
-
-        public GenericPair(T valueOne, Y valueTwo)
-        {
-            itemOne = valueOne;
-            itemTwo = valueTwo;
-        }
-
-        public T getOne()
-        {
-            return itemOne;
-        }
-        public Y getTwo()
-        {
-            return itemTwo;
-        }
-
-    }
     class Program
     {
         static void testGenericClass()
@@ -108,7 +21,7 @@ namespace S210105_Alorithms_and_Data_structures
             doubleGenericClass.getType(200.63);
 
             MyGenericClass<char> charGenericClass = new MyGenericClass<char>('i');
-            charGenericClass.getType('j');
+            charGenericClass.getType('k');
 
             MyGenericClass<float> floatGenericClass = new MyGenericClass<float>(5.3f);
             floatGenericClass.getType(156.9f);
@@ -176,8 +89,113 @@ namespace S210105_Alorithms_and_Data_structures
             testStack();
             Console.WriteLine();
 
+            MyLinkedList<string> testList = new MyLinkedList<string>();
+            testList.add("hello");
+            testList.add("world");
+            testList.add("wow");
+            testList.add("test");
+            testList.printList();
+            Console.WriteLine();
+            testList.insert("you", 2);
+            testList.printList();
+            Console.WriteLine();
+            testList.remove(3);
+            testList.printList();
+            Console.WriteLine();
+            testList.removeLast();
+            testList.printList();
+
+
             Console.ReadLine();
         }
+    }
+
+    class MyGenericClass<T>
+    {
+        private T genericMemberVariable;
+
+        public MyGenericClass(T value)
+        {
+            genericMemberVariable = value;
+        }
+
+        public T getType(T genericParameter)
+        {
+            Console.WriteLine("Parameter type: {0}, value {1}", typeof(T).ToString(), genericParameter);
+            Console.WriteLine("Return type: {0}, value: {1}", typeof(T).ToString(), genericMemberVariable);
+            Console.WriteLine();
+            return genericMemberVariable;
+        }
+
+        public T addOne()
+        {
+            //This was usesd to help add 2 generic values together
+            //https://stackoverflow.com/questions/8122611/c-sharp-adding-two-generic-values
+            if (typeof(T) == typeof(char))
+            {
+                //done separately so it needs to be converted to unicode to add the one
+                dynamic temp1 = Convert.ToInt32(genericMemberVariable);
+                genericMemberVariable = Convert.ToChar(temp1 + 1);
+            }
+            else
+            {
+                //for strings it concantinates it to the end
+                //others it adds 1 to the current value
+                dynamic temp1 = genericMemberVariable;
+                dynamic temp2 = 1;
+
+                genericMemberVariable = temp1 + temp2;
+            }
+
+            return genericMemberVariable;
+        }
+        public T genericProperty { get; set; }
+    }
+
+    class GenericArray<T>
+    {
+        private T[] array;
+        //inpus the data into the array
+
+        public GenericArray(T[] initalArray)
+        {
+            array = initalArray;
+        }
+
+        public T getData(int columnOne)
+        {
+            if (columnOne < array.GetLength(0) && columnOne >= -1)
+            {
+                return array[columnOne];
+            }
+            else
+            {
+                Console.WriteLine("Error - outside range of data");
+                return default(T);
+            }
+        }
+    }
+
+    class GenericPair<T, Y>
+    {
+        private T itemOne;
+        private Y itemTwo;
+
+        public GenericPair(T valueOne, Y valueTwo)
+        {
+            itemOne = valueOne;
+            itemTwo = valueTwo;
+        }
+
+        public T getOne()
+        {
+            return itemOne;
+        }
+        public Y getTwo()
+        {
+            return itemTwo;
+        }
+
     }
 
     class Stack<T>
@@ -263,5 +281,153 @@ namespace S210105_Alorithms_and_Data_structures
     //        }
     //    }
     //}
+
+    class MyLinkedList<T>
+    {
+       private int count;
+        private MyNode<T> Head;
+        private MyNode<T> End;
+       public void add (T data)
+        {
+            MyNode<T> NewNode = new MyNode<T>();
+            NewNode.Data = data;
+
+            if (End != null)
+            {
+                End.NextNodeRef = NewNode;
+            }
+
+            if (Head == null)
+            {
+                Head = NewNode;
+            }
+
+            End = NewNode;
+
+            count++;
+        }
+
+        public void insert (T data, int place)
+        {
+            if (place > -1 && place < count)
+            {
+                MyNode<T> NewNode = new MyNode<T>();
+                NewNode.Data = data;
+
+                MyNode<T> currentPlace = Head;
+
+                if (place == 0)
+                {
+                    NewNode.NextNodeRef = Head;
+                    Head = NewNode;
+                }
+                else
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        if (i == place - 1)
+                        {
+                            NewNode.NextNodeRef = currentPlace.NextNodeRef;
+                            currentPlace.NextNodeRef = NewNode;
+                            break;
+                        }
+                        currentPlace = currentPlace.NextNodeRef;
+                    }
+                }
+                ++count;
+            }
+            else if(place == count)
+            {
+                add(data);
+            }
+            else
+            {
+                Console.WriteLine("outside range of data");
+            }
+        }
+
+        public void remove (int place)
+        {
+            if (place > -1 && place < count)
+            {
+                
+                MyNode<T> currentPlace = Head;
+                MyNode<T> nextPlace = Head.NextNodeRef;
+               
+
+                if (place == 0)
+                { 
+                    currentPlace.Data = default(T);
+                    Head = currentPlace.NextNodeRef;
+                    currentPlace.NextNodeRef = default;
+                }
+                else
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        if (i == place - 1)
+                        {
+                            currentPlace.NextNodeRef = nextPlace.NextNodeRef;
+                            nextPlace.Data = default(T);
+                            nextPlace.NextNodeRef = default;
+                            break;
+                        }
+                        currentPlace = nextPlace;
+                        nextPlace = nextPlace.NextNodeRef;
+                    }
+                }
+                --count;
+            }
+            else if (place == count)
+            {
+                removeLast();
+            }
+            else
+            {
+                Console.WriteLine("outside range of data");
+            }
+        }
+        
+        public void removeLast()
+        {
+            MyNode<T> temp = Head;
+            for (int i = 0; i < count; i++)
+            {
+                if (temp.NextNodeRef == End)
+                {
+                    break;
+                }
+                temp = temp.NextNodeRef;
+            }
+            End.Data = default;
+            End = temp;
+        }
+
+        public void moveUp(int currentPlace)
+        {
+
+        }
+
+        public void moveDown(int currentPlace)
+        {
+
+        }
+
+        public void printList()
+        {
+            MyNode<T> temp = Head; 
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(temp.Data);
+                temp = temp.NextNodeRef;
+            }
+        }
+    }
+
+    class MyNode<T>
+    {
+        public T Data;
+        public MyNode<T> NextNodeRef;
+    }
 
 }
